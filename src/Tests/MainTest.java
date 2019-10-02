2d176564fbb6e22d47a1d2012d50a3880a55f274
Jamison Engels <engelsj@tcd.ie>
2019-10-02 14:16:56 +0100

    Added more test cases with looped trees

--- src/Tests/MainTest.java
+++ src/Tests/MainTest.java
@@ -55,4 +55,69 @@ public class MainTest {
         Node root = new Node(1,rightChild,leftChild);
         Assert.assertEquals(solution.lowestCommonAncestor(root,rightChild,leftChild), leftChild);
     }
+
+    // test with same children value
+    @Test
+    public void testRootWithSameChildValue(){
+        DevTaskOneSolution solution = new DevTaskOneSolution();
+        Node rightChild = new Node(2);
+        Node leftChild = new Node(2);
+        Node root = new Node(1,rightChild,leftChild);
+        Assert.assertEquals(solution.lowestCommonAncestor(root,rightChild,leftChild), root);
+    }
+
+    // test with same value as children value
+    @Test
+    public void testRootWithSameValueAsChildren(){
+        DevTaskOneSolution solution = new DevTaskOneSolution();
+        Node rightChild = new Node(2);
+        Node leftChild = new Node(2);
+        Node root = new Node(2,rightChild,leftChild);
+        Assert.assertEquals(solution.lowestCommonAncestor(root,rightChild,leftChild), root);
+    }
+
+    // test with loopedChildren
+    @Test
+    public void testRootWithLoopedChildren(){
+        DevTaskOneSolution solution = new DevTaskOneSolution();
+        Node rightChild = new Node(1);
+        Node root = new Node(1,rightChild,rightChild);
+        Assert.assertEquals(solution.lowestCommonAncestor(root,rightChild,rightChild), root);
+    }
+
+    // test with looped tree
+    @Test
+    public void testRootWithLoopedTree(){
+        DevTaskOneSolution solution = new DevTaskOneSolution();
+        Node rightChild = new Node(1);
+        Node root = new Node(1,rightChild,rightChild);
+        rightChild.right = root;
+        Assert.assertEquals(solution.lowestCommonAncestor(root,rightChild,rightChild), root);
+    }
+
+    // test with looped tree
+    @Test
+    public void testRootWithLoopedChildTree(){
+        DevTaskOneSolution solution = new DevTaskOneSolution();
+        Node rightChild = new Node(1);
+        Node leftChild = new Node(2);
+        rightChild.left = leftChild;
+        leftChild.right = rightChild;
+        Node root = new Node(1,rightChild,leftChild);
+        rightChild.right = root;
+        Assert.assertEquals(solution.lowestCommonAncestor(root,leftChild,rightChild), root);
+    }
+
+    // test with looped tree
+    @Test
+    public void testRootWithRootParameter(){
+        DevTaskOneSolution solution = new DevTaskOneSolution();
+        Node rightChild = new Node(1);
+        Node leftChild = new Node(2);
+        Node root = new Node(3,rightChild,leftChild);
+        rightChild.right = root;
+        Assert.assertEquals(solution.lowestCommonAncestor(root,leftChild,root), root);
+    }
+
+
 }
